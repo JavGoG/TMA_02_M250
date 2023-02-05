33b7ac13ac8307d15bd28b8bf39f0a56b06cb212
JavGog <javierggjavier@gmail.com>
2023-02-05 15:48:17 +0000

    (1)(d) New method ranDateInMonth().

--- src/main/java/BirthdaySimulation.java
+++ src/main/java/BirthdaySimulation.java
@@ -57,9 +57,70 @@ public class BirthdaySimulation
         }
         return month;
     }
+//    (1)(d) new ranDateInMonth method.
+    public int ranDateInMonth(String aMonth){
+
+        Random rand = new Random();
+        int x = rand. nextInt(4);
+        int y = rand. nextInt(30);
+        int z = rand. nextInt(29);
+        int number = rand.nextInt();
+
+        if(aMonth=="February"){
+
+            if(x==0){
+                return y;
+            }else{
+                return z;
+            }
+        }
+        switch (aMonth){
+            case   "January":
+            case   "March":
+            case   "May":
+            case   "July":
+            case   "September":
+            case   "November":
+                number = rand.nextInt(32);
+                break;
+            case "April":
+            case "June":
+            case "August":
+            case "October":
+            case "December":
+                number = rand.nextInt(31);
+                break;
+        }
+        System.out.println("Date: " + number);
+        return number;
+    }
 
     public BirthdaySimulation(){
         this.bdays = new ArrayList<String>();
     }
+
+    public static void main(String[] args) {
+        BirthdaySimulation birthdaySimulation = new BirthdaySimulation();
+        birthdaySimulation.ranDateInMonth("March");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
