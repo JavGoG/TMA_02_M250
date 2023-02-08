2018941467c608e06282a20420ddba40f29ce690
JavGog <javierggjavier@gmail.com>
2023-02-08 14:13:22 +0000

    (1)(f) new foundMatch method.

--- src/main/java/BirthdaySimulation.java
+++ src/main/java/BirthdaySimulation.java
@@ -4,6 +4,8 @@
  *
  * @author (your name)
  * @version (a version number or a date)
+ * <p>
+ * (1)(a) Adding import statements.
  */
 
 /**
@@ -18,69 +20,81 @@ import java.util.Random;
  *
  */
 
-public class BirthdaySimulation
-{
+public class BirthdaySimulation {
     public ArrayList<String> bdays;
 
-/**
- *  (1)(c) New 'ranMonth()' method.
- */
-    public String ranMonth(){
+    /**
+     *  (1)(c) New 'ranMonth()' method.
+     */
+    public String ranMonth() {
         Random rand = new Random();
-        int x = rand. nextInt(12);
+        int x = rand.nextInt(12);
         String month = "";
-        switch (x){
-            case 0: month = "January";
-            break;
-            case 1: month = "February";
-            break;
-            case 2: month = "March";
-            break;
-            case 3: month = "April";
-            break;
-            case 4: month = "May";
-            break;
-            case 5: month ="June" ;
-            break;
-            case 6: month = "July";
-            break;
-            case 7: month = "August";
-            break;
-            case 8: month = "September";
-            break;
-            case 9: month = "October";
-            break;
-            case 10: month ="November";
-            break;
-            case 11: month = "December";
-            break;
+        switch (x) {
+            case 0:
+                month = "January";
+                break;
+            case 1:
+                month = "February";
+                break;
+            case 2:
+                month = "March";
+                break;
+            case 3:
+                month = "April";
+                break;
+            case 4:
+                month = "May";
+                break;
+            case 5:
+                month = "June";
+                break;
+            case 6:
+                month = "July";
+                break;
+            case 7:
+                month = "August";
+                break;
+            case 8:
+                month = "September";
+                break;
+            case 9:
+                month = "October";
+                break;
+            case 10:
+                month = "November";
+                break;
+            case 11:
+                month = "December";
+                break;
         }
         return month;
     }
-//    (1)(d) new ranDateInMonth method.
-    public int ranDateInMonth(String aMonth){
+
+    //    (1)(d) new ranDateInMonth method.
+    public int ranDateInMonth(String aMonth) {
 
         Random rand = new Random();
-        int x = rand. nextInt(4);
-        int y = rand. nextInt(30);
-        int z = rand. nextInt(29);
+        int x = rand.nextInt(4);
+        int y = rand.nextInt(30);
+        int z = rand.nextInt(29);
         int number = rand.nextInt();
 
-        if(aMonth=="February"){
+        if (aMonth == "February") {
 
-            if(x==0){
+            if (x == 0) {
                 return y;
-            }else{
+            } else {
                 return z;
             }
         }
-        switch (aMonth){
-            case   "January":
-            case   "March":
-            case   "May":
-            case   "July":
-            case   "September":
-            case   "November":
+        switch (aMonth) {
+            case "January":
+            case "March":
+            case "May":
+            case "July":
+            case "September":
+            case "November":
                 number = rand.nextInt(32);
                 break;
             case "April":
@@ -94,15 +108,29 @@ public class BirthdaySimulation
         System.out.println("Date: " + number);
         return number;
     }
-//    (1)(e) new createMeeting method.
-    public void createMeeting(int numAttendees){
+
+    //    (1)(e) new createMeeting method.
+    public void createMeeting(int numAttendees) {
         bdays.clear();
         BirthdaySimulation birthdaySimulation = new BirthdaySimulation();
         String bday = "" + numAttendees + " " + birthdaySimulation.ranMonth();
         bdays.add(bday);
         System.out.println(bday);
     }
-    public BirthdaySimulation(){
+
+    // (1)(f) new foundMatch method.
+    public boolean foundMatch() {
+        boolean bool = false;
+        for (int i = 0; i < bdays.size(); i++) {
+            for (int j = 1; j < bdays.size(); j++) {
+                if (bdays.get(i) == bdays.get(j)) {
+                    return !bool;
+                }
+            }
+        }
+        return bool;
+    }
+    public BirthdaySimulation() {
         this.bdays = new ArrayList<String>();
     }
 
@@ -113,22 +141,6 @@ public class BirthdaySimulation
         System.out.println(date);
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
